112dd64f3714a9158efbb22d344bc556f201a26e
Omar Alomar <omaro2000@gmail.com>
2022-01-02 00:16:41 -0500

    added food logic

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
+import { update as updateFood, draw as drawFood } from './food.js'
 
 let lastRenderTime = 0;
 const gameBoard = document.getElementById('game-board');
@@ -17,10 +18,12 @@ function main(currentTime) {
 window.requestAnimationFrame(main);
 
 function update () {
-    updateSnake();
+    updateSnake()
+    updateFood()
 }
 
 function draw () {
     gameBoard.innerHTML = ''
     drawSnake(gameBoard);
+    drawFood(gameBoard);
 }
